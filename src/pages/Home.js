8616598d15b1edb84e4dc018c173fba080d613a9
Ayim William <odhisayim@gmail.com>
2024-08-08 15:20:50 +0300

    added parent component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import UserCard from "../components/UserCard";
-import NavBar from "../components/NavBar";
+
 
 function Home() {
   const [users, setUsers] = useState([]);
@@ -19,7 +19,7 @@ function Home() {
   return (
     <>
       <header>
-        <NavBar />
+        
       </header>
       <main>
         <h1>Home!</h1>
